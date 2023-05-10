9e8c330f5f692c0cbdc9df30a9007daeb7376550
meshkov vladislav <meshkov.vs@phystech.edu>
2023-05-10 22:09:07 +0300

    edit extract_age

--- extract_age.sql
+++ extract_age.sql
@@ -1,7 +1,12 @@
-	
 select
 	staff_id
 	, name
-	, ((CURRENT_DATE - birthdate) / 365) as age
+	, case 
+	  when extract(month from CURRENT_DATE)>extract(month from birthdate) then 
+			 	extract(year from CURRENT_DATE)-extract(year from birthdate)
+	    when extract(month from CURRENT_DATE)=extract(month from birthdate)
+			and extract(day from CURRENT_DATE)>=extract(day from birthdate) then
+				extract(year from CURRENT_DATE)-extract(year from birthdate)
+		else extract(year from CURRENT_DATE)-extract(year from birthdate) - 1
+		end as age
 from Staff
-;
